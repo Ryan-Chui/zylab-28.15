2042df0e7ff2c0fd3c3cea4170c51b2d2e161602
Ryan-Chui <152207388+Ryan-Chui@users.noreply.github.com>
2023-11-30 10:14:38 -0600

    Update PlaylistNode.h

--- PlaylistNode.h
+++ PlaylistNode.h
@@ -1,1 +1,29 @@
+/* Type your code here */
+#ifndef PLAYLISTNODEH
+#define PLAYLISTNODEH
 
+#include <string>
+using namespace std;
+
+class PlaylistNode {
+	private:
+		string uniqueID;
+   string songName;
+   string artistName;
+   int songLength;
+   PlaylistNode* nextNodePtr;
+
+	public:
+	   PlaylistNode();
+	   PlaylistNode(string id, string name, string artist, int length, PlaylistNode* node);
+		string GetID();
+      string GetSongName();
+      string GetArtistName();
+      int GetSongLength();
+      PlaylistNode* GetNext();
+      void InsertAfter(PlaylistNode* nodePtr);
+      void SetNext(PlaylistNode* nodePtr);
+      void PrintPlaylistNode();
+};
+
+#endif
