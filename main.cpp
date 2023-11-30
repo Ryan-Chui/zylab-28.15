ba8d8837a58c440c44fb8cdb7a70cb227b0147c5
Ryan-Chui <152207388+Ryan-Chui@users.noreply.github.com>
2023-11-30 13:02:36 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
-#include "PlaylistNode.h"
+#include "SongNode.h"
 
-using namespace std;
-
-void PrintMenu(const string playlistTitle) {
-   /* Type your code here */
-   
+// TODO: Write PrintPlaylist() function
+void PrintPlaylist(SongNode* HeadNode){
+   SongNode* currNode = HeadNode->GetNext();
+	while (currNode != NULL) {
+		currNode->PrintSongInfo();
+		if(currNode->GetNext() != nullptr){
+		cout << endl;}
+		currNode = currNode->GetNext();
+	}
 }
 
-PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
-   /* Type your code here */
-   
-}
 
 int main() {
-   /* Type your code here */
-   
-   return 0;
-}
+	SongNode* headNode;
+	SongNode* currNode;
+	SongNode* lastNode;
 
+	string songTitle;
+	string songLength;
+	string songArtist;
+
+	// Front of nodes list                                                                         
+	headNode = new SongNode();
+	lastNode = headNode;
+
+	// Read user input until -1  entered
+	getline(cin, songTitle);
+	while (songTitle != "-1") {
+		getline(cin, songLength);
+		getline(cin, songArtist);
+
+		currNode = new SongNode(songTitle, songLength, songArtist);
+		lastNode->InsertAfter(currNode);
+		lastNode = currNode;
+
+		getline(cin, songTitle);
+	}
+
+	// Print linked list
+	cout << "LIST OF SONGS" << endl;
+	cout << "-------------" << endl;
+	PrintPlaylist(headNode);
+	
+	return 0;
+}
