7b48d83ad36441f5b7206cc0fda82f97a06cec34
Ryan-Chui <152207388+Ryan-Chui@users.noreply.github.com>
2023-12-03 14:29:55 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,22 +20,74 @@ void PrintMenu(const string playlistTitle) {
 
 PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
    /* Type your code here */
+   string id;
+    string name;
+    string artist;
+    int length;
+    PlaylistNode* currNode;
 switch (option) {
   case 'a':
     
+    cout << "enter song id" << endl;
+    getline(cin, id);
+    cout << "enter song name" << endl;
+    getline(cin, name);
+    cout << "enter song artist" << endl;
+    getline(cin , artist);
+    PlaylistNode* currNode;
+    currNode = new PlaylistNode(id, name, artist, length, null);
+    headNode->InsertAfter(currNode);
     break;
+    
+    
   case 'd':
+      
+  cout << "enter id for the song you want to delete" << endl;
+  cin >> id;
+   currNode = HeadNode;
+  while(currNode->GetNext() != NULL){
+      if(currNode->GetNext.GetID() == id){
+           currNode.setNext(currNode->GetNextPtr().GetNextPtr());
+           break;
+      }
+      currNode = currNode->GetNext();
+  }
     break;
   case 'c':
+  
     break;
   case 's':
+  cin >> artist;
+  currNode = HeadNode;
+  while (currNode->GetNext != NULL){
+     if(currNode->GetArtistName() == artist){
+         currNode->PrintPlaylistNode();
+     }
+     currNode = currNode->GetNext();
+     
+  }
     break;
   case 't':
+  int seconds = 0;
+  currNode = HeadNode;
+  while (currNode->getNext() != null){
+        seconds+= currNode->SongLength();
+        currNode = currNode->GetNext();
+  }
     break;
   case 'o':
+  currNode = HeadNode->GetNext();
+  while(currNode != NULL){
+      currNode->PrintPlaylistNode();
+      currNode = currNode->GetNext();
+  }
     break;
   case 'q':
     break;
+   case default:
+   cout << "please choose an option";
+   cin >> input;
+   ExcecuteMenu(input, playlistName, headNode);
    
 }
 
@@ -55,4 +107,3 @@ int main() {
    cin >> input;
    return 0;
 }
-
